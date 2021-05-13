b13eac130fcce65e451e3592cbee576e38ce6514
Kurdi Botond <kurdi.boti@gmail.com>
2021-05-13 17:39:13 +0200

    firebase crashlytics added, intentional crash

--- app/src/main/java/aut/bme/bookmanager/ui/about/AboutFragment.kt
+++ app/src/main/java/aut/bme/bookmanager/ui/about/AboutFragment.kt
@@ -6,14 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import aut.bme.bookmanager.R
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        error_btn.setOnClickListener {
+            throw RuntimeException("Click button on about page.")
+        }
     }
 }
